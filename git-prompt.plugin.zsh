eadc21695d4f2e784b66152b73ecce5f3ba0ab05
Michael Faas <mfaas@revision21.com>
2017-06-29 14:12:54 -0400

    Slight prompt color changes

--- git-prompt.plugin.zsh
+++ git-prompt.plugin.zsh
@@ -97,8 +97,8 @@ git_super_status() {
 }
 
 # Default values for the appearance of the prompt. Configure at will.
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:["
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg_bold[blue]%}]%{$reset_color%} ➜ "
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[yellow]%}[ "
+ZSH_THEME_GIT_PROMPT_SUFFIX=" %{$fg_bold[yellow]%}] "
 ZSH_THEME_GIT_PROMPT_SEPARATOR=" |"
 ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
 ZSH_THEME_GIT_PROMPT_STAGED=" %{$fg[red]%}%{●%G%}"
